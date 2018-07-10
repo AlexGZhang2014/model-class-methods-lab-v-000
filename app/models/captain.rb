e96ac5aac734bb4ed07854c988a8cb8c1494ae8e
Alex Zhang <alexgzhang92@gmail.com>
2018-07-10 06:37:19 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -4,6 +4,10 @@ class Captain < ActiveRecord::Base
   def self.catamaran_operators
     includes(boats: :classifications).where(:classifications => {:name => "Catamaran"})
   end
+  
+  def self.sailors
+    includes(boats: :classifications).where(classifications: {name: "Sailboat"})
+  end
 end
 
 describe Captain do
