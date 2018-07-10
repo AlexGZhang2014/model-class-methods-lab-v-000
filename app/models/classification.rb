6fdf493058f7607f667b5ac2b99d6d4d07ecd1eb
Alex Zhang <alexgzhang92@gmail.com>
2018-07-10 06:49:57 +0000

    automatically backed up by learn

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -5,13 +5,6 @@ class Classification < ActiveRecord::Base
   def self.my_all
     all.uniq
   end
-  
-  describe "::my_all" do
-    it "returns all classifications" do
-      classifications = ["Ketch", "Sailboat", "Catamaran", "Sloop", "Motorboat", "Center Console", "RIB", "Trawler", "Cat Rig Boat", "Bass Boat", "Pontoon Boat"]
-      expect(Classification.my_all.pluck(:name).uniq).to eq(classifications)
-    end
-  end
 
   describe "#longest" do
     it "returns the classifications for the longest boat" do
