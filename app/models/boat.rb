0811d52e574b88bf68500cbfefcaaa08ab6c0315
Alex Zhang <alexgzhang92@gmail.com>
2018-07-10 05:35:15 +0000

    automatically backed up by learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,6 +2,8 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+  
+  
 end
 
 describe Boat do
